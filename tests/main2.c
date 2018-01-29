0ff4aed773aec06782652beadc56e81830ca85da
Romeo Lossy <rlossy@z1r13p3.le-101.fr>
2018-01-29 13:52:37 +0100

    rm all

--- tests/main2.c
+++ tests/main2.c
@@ -6,7 +6,7 @@
 /*   By: rlossy <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/18 15:12:38 by rlossy       #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/24 15:42:18 by rlossy      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/29 13:49:11 by rlossy      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -52,7 +52,91 @@ int main(void)
 	printf("\nlen = %d\n", ft_printf("%hC, %hC", 0, L'猫'));
 	*/
 //	printf("\n%s\n", "------PRINTF------");
-	printf("\nlen = %d\n", printf("test : %10%"));
+//	printf("\nlen = %d\n", printf("test : %10%"));
 //	printf("\n%s\n", "------FT_PRINTF------");
-	printf("\nlen = %d\n", ft_printf("test : %10%"));
+//	printf("\nlen = %d\n", ft_printf("test : %10%"));
+/*
+	printf("\n%s\n", "------PRINTF------");
+	printf("\nlen = %d\n", printf("%###-#0000 33...12..#0+0d", 256));
+	printf("\n%s\n", "------FT_PRINTF------");
+	printf("\nlen = %d\n", ft_printf("%###-#0000 33...12..#0+0d", 256));
+
+	printf("\n%s\n", "------PRINTF------");
+	printf("\nlen = %d\n", printf("%####0000 33..1..#00d\n", 256));
+	printf("\n%s\n", "------FT_PRINTF------");
+	printf("\nlen = %d\n", ft_printf("%####0000 33..1..#00d\n", 256));
+
+	printf("\n%s\n", "------PRINTF------");
+	printf("\nlen = %d\n", printf("{%05.*d}", -15, 42));
+	printf("\n%s\n", "------FT_PRINTF------");
+	printf("\nlen = %d\n", ft_printf("{%05.*d}", -15, 42));
+	*//*
+	printf("\n%s\n", "------PRINTF------");
+	printf("\nlen = %d\n", printf("%zhd", 4294967296));
+	printf("\n%s\n", "------FT_PRINTF------");
+	printf("\nlen = %d\n", ft_printf("%zhd", 4294967296));
+
+	printf("\n%s\n", "------PRINTF------");
+	printf("\nlen = %d\n", printf("%jhd", 9223372036854775807));
+	printf("\n%s\n", "------FT_PRINTF------");
+	printf("\nlen = %d\n", ft_printf("%jhd", 9223372036854775807));
+*/
+
+//	printf("\n%s\n", "------PRINTF------");
+//	printf("\nlen = %d\n", printf("{%.4S}", L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B"));
+//	printf("\n%s\n", "------FT_PRINTF------");
+//	printf("\nlen = %d\n", ft_printf("{%.4S}", L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B"));
+
+//	printf("\n%s\n", "------PRINTF------");
+//	printf("\nlen = %d\n", printf("%15.4S", L"我是一只猫。"));
+//	printf("\n%s\n", "------FT_PRINTF------");
+//	printf("\nlen = %d\n", ft_printf("%15.4S", L"我是一只猫。"));
+//	printf("\n%s\n", "------PRINTF------");
+//	printf("\nlen = %d\n", printf("{%.*s}", -1, "42"));
+//	printf("\n%s\n", "------FT_PRINTF------");
+//	printf("\nlen = %d\n", ft_printf("{%.*s}", -1, "42"));
+
+/*
+	printf("\nlen = %d\n", printf("%C", (wint_t)-2));
+	printf("\nlen = %d\n", ft_printf("%C", (wint_t)-2));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("%C", 0xd800));
+	printf("\nlen = %d\n", ft_printf("%C", 0xd800));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("%C", 0xdb02));
+	printf("\nlen = %d\n", ft_printf("%C", 0xdb02));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("%C", 0xdfff));
+	printf("\nlen = %d\n", ft_printf("%C", 0xdfff));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("%C", 0xbffe));
+	printf("\nlen = %d\n", ft_printf("%C", 0xbffe));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("%lc", 254));
+	printf("\nlen = %d\n", ft_printf("%lc", 254));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("%C", 256));
+	printf("\nlen = %d\n", ft_printf("%C", 256));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("%8C et coco %C titi %lc", 3250, 0x11ffff, 'a'));
+	printf("\nlen = %d\n", ft_printf("%8C et coco %C titi %lc", 3250, 0x11ffff, 'a'));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("%6C et coco %C titi %C tyty", 3250, 0xd800, 'u'));
+	printf("\nlen = %d\n", ft_printf("%6C et coco %C titi %C tyty", 3250, 0xd800, 'u'));
+	printf("\n-------------NEXT TEST------------------\n");
+
+	printf("\nlen = %d\n", printf("yo%2C%-12lc et %C titi %C tyty", 'u', 254, 256, 'a'));
+	printf("\nlen = %d\n", ft_printf("yo%2C%-12lc et %C titi %C tyty", 'u', 254, 256, 'a'));*/
+
+	printf("\nlen = %d\n", printf("%.1f", 5.2));
+	printf("\nlen = %d\n", ft_printf("%.1f", 5.2));
+
 }
